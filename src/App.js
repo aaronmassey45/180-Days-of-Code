8805d6973bde3d87e85c9708361e4e17710583c2
Aaron <aaronmassey45@gmail.com>
2018-06-08 09:28:14 -0500

    Moved markdown into App component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
-import React, { Component } from 'react';
 import './App.css';
-import Navbar from './components/navbar';
-import Markdown from './components/markdown';
+import React, { Component } from 'react';
+import marked from 'marked';
+import Navbar from './navbar';
 
 class App extends Component {
+  state = {
+    value:
+      'Heading\n=======\n\nSub-heading\n-----------\n \n### Another deeper heading\n \nParagraphs are separated\nby a blank line.\n\nLeave 2 spaces at the end of a line to do a  \nline break\n\nText attributes *italic*, **bold**, \n`monospace`, ~~strikethrough~~ .\n\nShopping list:\n\n  * apples\n  * oranges\n  * pears\n\nNumbered list:\n\n  1. apples\n  2. oranges\n  3. pears\n\nThe rain---not the reign---in\nSpain.\n\n *[Herman Fassett](https://www.freecodecamp.org/hermanfassett)*',
+  };
+
+  handleChange = event => {
+    this.setState({ value: event.target.value });
+  };
+
   render() {
     return (
       <div className="App">
         <Navbar />
-        <Markdown />
+        <div
+          className="container row"
+          style={{ marginRight: 'auto', marginLeft: 'auto' }}
+        >
+          <div className="col-sm-6">
+            <textarea value={this.state.value} onChange={this.handleChange} />
+          </div>
+          <div
+            className="col-sm-6"
+            dangerouslySetInnerHTML={{ __html: marked(this.state.value) }}
+          />
+        </div>
       </div>
     );
   }
